6090b9f05d77ad4d3eff7d86859b5f0b6e589918
JackieChengZhang <zc1063870329@163.com>
2025-04-27 08:53:55 +0000

    Deploying to gh-pages from @ JackieChengZhang/JackieChengZhang.github.io@c7e5c70fc5904614da63c07a00db9a1c2afeb1f7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,11 +398,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-announcement-1",
-          title: 'Announcement_1',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_1/";
             },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
